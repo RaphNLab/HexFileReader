e65e432b962c023f36945059d2a0357e2d1e3728
raphnlab <s.ngoufack@raphnlab.de>
2025-02-13 21:23:27 +0100

    Minimal change

--- hex_file_reader.c
+++ hex_file_reader.c
@@ -66,7 +66,7 @@ void MemRelease(char **buffer, uint32_t col);
 
 int main(void)
 {
-    char *fileName = "Input_file/PC550_STM32.hex";
+    char *fileName = "Input_File/PC550_STM32.hex";
     char fileData[FILE_SIZE] = {0};
     
     //size_t cnt = 0;
@@ -80,8 +80,9 @@ int main(void)
 
     readHexFileLine(fileLines, hexData, fileName);
 
-    uint8_t dataUint[16] = {0};
+    /*uint8_t dataUint[16] = {0};
     getByteValue(fileData, dataUint);
+    */
 
     MemRelease(hexData, HEX_FILE_MAX_COL);    
     return 0;
@@ -106,7 +107,8 @@ void MemRelease(char **buffer, uint32_t col)
 {
     for(uint32_t i = 0; i < col; i++)
     {
-       free(buffer[i]);
+        char *ptr = buffer[i];
+       free(ptr);
     }
     free(buffer);
 }
